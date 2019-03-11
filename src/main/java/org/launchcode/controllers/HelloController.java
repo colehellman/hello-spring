44ab337741377031933640b4efa229056c5d20f4
Cole Hellman <cole.hellman@gmail.com>
2019-03-11 16:10:28 -0500

    class 3 studio

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,27 +25,47 @@ public class HelloController {
         return "goodbye";
     }
 
-//    @RequestMapping(value = "hello", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String helloForm(){
-//        String html = "<form method='post'>" +
-//                "<input type='text' name='name' />" +
-//                "<input type='submit' value='Greet Me!'/>" +
-//                "</form>";
-//        return html;
-//    }
-//
-//    @RequestMapping(value = "hello", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String helloPost(HttpServletRequest request){
-//        String name = request.getParameter("name");
-//        return "Hello " + name;
-//    }
-//
-//    @RequestMapping(value = "hello/{name}")
-//    @ResponseBody
-//    public String helloUrlSegment(@PathVariable String name){
-//        return "Hello " + name;
-//    }
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm(){
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<select id='language' name='language'><option value=''>Pick a language</option><option value='french'>French</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='chinese'>Chinese</option>" +
+                "<option value='korean'>Korean</option>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String language = request.getParameter("language");
+        if (language.equals("french")) {
+            return "Bonjour " + name;
+        }
+        if (language.equals("spanish")) {
+            return "Hola " + name;
+        }
+        if (language.equals("german")) {
+            return "Hallo " + name;
+        }
+        if (language.equals("chinese")) {
+            return "Ni hao " + name;
+        }
+        else {
+            return "Yeoboseyo " + name;
+        }
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name){
+        return "Hello " + name;
+    }
 
 }
